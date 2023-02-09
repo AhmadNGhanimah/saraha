c984634473fa3b34b60c0d4247a9d9a76aa777e0
Tanygt <ahmad.ghanimah197@gmail.com>
2023-02-09 03:35:26 +0300

    auth + image to registar

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.master')
+@section('title')
+Home
+@endsection
+@section('content')
+<section>
+    <h1>layout and navbar is well done </h1>
+</section>
+@endsection
+
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -4,13 +4,13 @@
         <div class=" float-lg-right" id="navbarText">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
-                    <a class="nav-link nav-link-color" href="#">Login <span class="sr-only">(current)</span></a>
+                    <a class="nav-link nav-link-color" href="{{ route('login') }}">Login <span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">On WebSite</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Registar</a>
+                    <a class="nav-link" href="{{ route('register') }}">Registar</a>
                 </li>
                 <li class="nav-item ">
                     <a class="nav-link" href="#">Contact Us</a>
