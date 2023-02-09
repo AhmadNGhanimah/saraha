1a05ec4faa4c83f441e1982025446cd283f11baf
Tanygt <ahmad.ghanimah197@gmail.com>
2023-02-09 03:58:20 +0300

    added file (Ar , En)

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-light" style="background: #10bbb3">
-    <div class="container-fluid">
+    <div class="container-fluid navbar-saraha">
         <a class=" name-of-logo" href="#">Saraha</a>
         <div class=" float-lg-right" id="navbarText">
             <ul class="navbar-nav mr-auto">
@@ -15,6 +15,13 @@
                 <li class="nav-item ">
                     <a class="nav-link" href="#">Contact Us</a>
                 </li>
+
+                <select class="changelang">
+{{--                    just for design--}}
+                    <option value="1" >Arabic</option>
+                    <option value="2">English</option>
+                </select>
+
 
             </ul>
         </div>
