3b732e276d29c0e96507fc451d08c3cb1fc419ac
WaleedH <waleed_hamdan88@outlook.com>
2023-03-26 00:10:18 +0300

    lang

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -54,7 +54,7 @@
                         <div class="row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Reset Password') }}
+                                    {{ __('custom.resetPassword') }}
                                 </button>
                             </div>
                         </div>
